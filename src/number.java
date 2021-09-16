38e7818e0eaee4366bf57e93b39c2af958425cd5
batuanr <88823692+batuanr@users.noreply.github.com>
2021-09-16 19:10:42 +0700

    xong

---
+++ src/number.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input number");
+        int number = scanner.nextInt();
+        if (number < 13) {
+            System.out.println(check13ToDown(number));
+        }
+        else {
+            if (number < 100) {
+                System.out.println(check100ToDown(number));
+            }
+            else {
+                System.out.println(check999ToDown(number));
+            }
+        }
+    }
+    static String check13ToDown(int n){
+        switch(n){
+            case 1:
+                return "one";
+
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "nine";
+            case 10:
+                return "ten";
+            case 11:
+                return "eleven";
+            case 12:
+                return "twelve";
+        }
+        return "";
+    }static String check20ToDown(int n){
+        switch(n){
+            case 2:
+                return "twen";
+            case 3:
+                return "thir";
+
+            case 4:
+                return "four";
+            case 5:
+                return "fif";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eigh";
+            case 9:
+                return "nine";
+        }
+        return null;
+    }
+    static String check100ToDown(int n){
+        int a = (n -n%10)/10;
+        int b = n%10;
+        if (b==0){
+            return check20ToDown(a)+"ty";
+        }
+        else{
+            if (a < 2){
+                return check20ToDown(b)+"teen";
+            }return check20ToDown(a)+"ty-"+check13ToDown(b);
+        }
+
+    }
+    static String check999ToDown(int n){
+        int a = (n -n%100)/100;
+        int b = n%100;
+        if (b<13){
+            return check13ToDown(a)+" hundred "+check13ToDown(b);
+        }
+        return check13ToDown(a)+" hundred "+check100ToDown(b);
+    }
+}
